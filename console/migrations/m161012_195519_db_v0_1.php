32f55742947aba7119c0e56bacb90debbc1e7dcc
reyzeer <czarneckizbigniew0@gmail.com>
2016-10-12 21:56:46 +0200

    Prepared tables.

---
+++ console/migrations/m161012_195519_db_v0_1.php
@@ -0,0 +1,29 @@
+<?php
+
+use yii\db\Migration;
+
+class m161012_195519_db_v0_1 extends Migration
+{
+    public function up()
+    {
+
+    }
+
+    public function down()
+    {
+        echo "m161012_195519_db_v0_1 cannot be reverted.\n";
+
+        return false;
+    }
+
+    /*
+    // Use safeUp/safeDown to run migration code within a transaction
+    public function safeUp()
+    {
+    }
+
+    public function safeDown()
+    {
+    }
+    */
+}
