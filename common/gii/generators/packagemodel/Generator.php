b916b5ac4dca76dfb6b1eed926e71bcd88356351
reyzeer <czarneckizbigniew0@gmail.com>
2016-10-21 23:38:30 +0200

    Model package generator - start

--- common/gii/generators/packagemodel/Generator.php
+++ common/gii/generators/packagemodel/Generator.php
@@ -2,16 +2,15 @@
 
 namespace common\gii\generators\packagemodel;
 
+use \yii\gii\generators\model\Generator as ModelGenerator;
+
 /**
- * Created by PhpStorm.
- * User: reyzeer
- * Date: 12.10.16
- * Time: 23:01
+ * TODO
  */
-class Generator extends \yii\gii\Generator
+class Generator extends ModelGenerator
 {
 
-    public $asd;
+    public $packageName;
 
     /**
      * @inheritdoc
@@ -26,7 +25,7 @@ class Generator extends \yii\gii\Generator
      */
     public function getDescription()
     {
-        return 'This generator generates an ActiveRecord class, specfic object and search class class for the specified database table. ';
+        return 'This generator generates an ActiveRecord class, specfic object and search class class for the specified database table.';
     }
 
     /**
